22b5197d7de29a76cf91beb2104fec05de075860
Afrographix <bruneltesse@gmail.com>
2026-06-06 21:59:05 +0100

    new deploy

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("push", (event) => {
+  const data = event.data.json();
+
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: data.icon,
+    badge: data.badge,
+  });
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow("/messages")
+  );
+});
