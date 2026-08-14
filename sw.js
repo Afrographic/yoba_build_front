97bf479be649649fa9ae42e713da549f36219b5e
Afrographix <bruneltesse@gmail.com>
2026-08-14 13:58:20 +0100

    upload

--- sw.js
+++ sw.js
@@ -1,23 +1,12 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
+self.addEventListener('push', function(event) {
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(clients.claim());
-});
+    const data = event.data.json();
 
-self.addEventListener("push", (event) => {
-  const data = event.data.json();
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: '/icons/Icon-192.png'
+        })
+    );
 
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    // icon: data.icon,
-    // badge: data.badge,
-  });
-});
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-
-  event.waitUntil(clients.openWindow("/#/contacts?contacts=true"));
 });
